26dae5ae19486fafcabd4ce7fa46a13edd79c302
dirasharajbhandari <dirasharajbhandari@gmail.com>
2026-05-27 18:18:45 +0545

    again changed

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
@@ -6,12 +5,3 @@ const config = {
 };
 
 export default config;
-=======
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
-
-export default config;
->>>>>>> 9824c831498c9a964d00686207506dd887c868fb
